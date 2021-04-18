361dfa1b6e7c3cb30cc790f841f0c3ba758ff10f
dillonhammer <dillonhammer@gmail.com>
2021-04-18 03:50:29 -0400

    Add antd + airhorns

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
 import ReactHowler from "react-howler";
+import styled from "styled-components";
+import { Button, Input } from "antd";
+import "antd/dist/antd.css";
 
 console.log("initializing socket");
 const socket = io("https://pit-applause-server.herokuapp.com");
+
+const WelcomeContainer = styled.div`
+  margin: 40px;
+  text-align: center;
+`;
+
+const GridContainer = styled.div`
+  display: flex;
+`;
+
+const RoomContainer = styled.div`
+  margin: 40px;
+`;
+
+const ButtonContainer = styled.div`
+  flex: 1;
+`;
 
 function App() {
   const [name, setName] = useState("");
   const [entered, setEntered] = useState(false);
   const [count, setCount] = useState(1);
   const [clapping, setClapping] = useState([]);
+  const [airhorns, setAirhorns] = useState([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
@@ -17,12 +38,14 @@ function App() {
       setEntered(true);
       setCount(payload.count);
       setClapping(payload.clapping);
+      setAirhorns(payload.airhorns);
     });
 
     socket.on("update", (payload) => {
       if (entered) {
         setCount(payload.count);
         setClapping(payload.clapping);
+        setAirhorns(payload.airhorns);
       }
     });
 
@@ -33,16 +56,20 @@ function App() {
 
   const onKeyPress = (event) => {
     if (event.key === "Enter") {
-      socket.emit("enter", name);
+      onEnter(name);
     }
   };
 
-  const onPlay = () => {
-    socket.emit("clap", name);
+  const onEnter = (name) => {
+    if (name) {
+      socket.emit("enter", name);
+    } else {
+      setError("Name cannot be empty");
+    }
   };
 
-  const onStop = () => {
-    socket.emit("end_clap", name);
+  const onSend = (event) => {
+    socket.emit(event, name);
   };
 
   const onMock = () => {
@@ -56,34 +83,69 @@ function App() {
   };
 
   return !entered ? (
-    <div>
-      Enter your name:
-      <input
+    <WelcomeContainer>
+      <p>Enter your name</p>
+      <Input
         onChange={({ target }) => setName(target.value)}
         onKeyPress={onKeyPress}
+        style={{ width: 200 }}
       />
+      <br />
+      <br />
+      <Button type="primary" onClick={() => onEnter(name)}>
+        Join Room
+      </Button>
       {error && <div>{error}</div>}
-    </div>
+    </WelcomeContainer>
   ) : (
-    <div>
-      <button onMouseDown={onPlay} onMouseUp={onStop}>
-        Play!
-      </button>
-      {name === "root" && (
-        <>
-          <button onMouseDown={onMock}>Mock Person</button>
-          <button onMouseDown={onClear}>Clear All</button>
-        </>
-      )}
-      <div>{count} here</div>
-      <div>{clapping.length} clapping</div>
-      {clapping.map((name) => (
-        <div key={name}>{name}</div>
-      ))}
-      {clapping.map((name) => (
-        <ReactHowler key={name} src="clapping.mp3" loop html5 />
-      ))}
-    </div>
+    <RoomContainer>
+      <p>{count} Here</p>
+      <GridContainer>
+        <ButtonContainer>
+          <Button
+            type="primary"
+            onMouseDown={() => onSend("clap")}
+            onMouseUp={() => onSend("end_clap")}
+          >
+            Hold to Clap
+          </Button>
+          <br />
+          <br />
+          <div>{clapping.length} Clapping</div>
+          {clapping.map((name) => (
+            <div key={name}>{name}</div>
+          ))}
+          {name === "root" && (
+            <>
+              <button onMouseDown={onMock}>Mock Person</button>
+              <button onMouseDown={onClear}>Clear All</button>
+            </>
+          )}
+        </ButtonContainer>
+        <ButtonContainer>
+          <Button
+            type="primary"
+            danger
+            onMouseDown={() => onSend("airhorn")}
+            onMouseUp={() => onSend("end_airhorn")}
+          >
+            Hold to Airhorn
+          </Button>
+          <br />
+          <br />
+          <div>{airhorns.length} Airhorns</div>
+          {airhorns.map((name) => (
+            <div key={name}>{name}</div>
+          ))}
+          {clapping.map((name) => (
+            <ReactHowler key={name} src="clapping.mp3" loop html5 />
+          ))}
+          {airhorns.map((name) => (
+            <ReactHowler key={name} src="airhorn.mp3" loop html5 />
+          ))}
+        </ButtonContainer>
+      </GridContainer>
+    </RoomContainer>
   );
 }
 
